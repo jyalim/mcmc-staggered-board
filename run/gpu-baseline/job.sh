7c11788570ffcd38fa4923d770aca868762cfbb5
Jason Yalim <jyalim@asu.edu>
2022-05-10 18:01:46 +0000

    init baselines

---
+++ run/gpu-baseline/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -N 1-1
+#SBATCH -c 32
+#SBATCH -t 15
+#SBATCH --export=NONE
+#SBATCH -o log/slurm.%j.out
+#SBATCH -e log/slurm.%j.err
+##SBATCH --gres=gpu:1          
+
+module purge
+
+module load rapidsai/21.06
+
+python likelifun1.py
